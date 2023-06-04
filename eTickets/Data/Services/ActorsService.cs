1548fc7822129811e0b523110f4944ace3673136
Shydeev <hday5297@gmail.com>
2023-06-04 17:09:58 +0300

    Add IActorsService and implement it.

---
+++ eTickets/Data/Services/ActorsService.cs
@@ -0,0 +1,41 @@
+﻿using eTickets.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace eTickets.Data.Services
+{
+    public class ActorsService : IActorsService
+    {
+        private readonly AppDbContext _context;
+
+        public ActorsService(AppDbContext context)
+        {
+            this._context = context;
+        }
+
+        public void Add(Actor actor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Actor>> GetAll()
+        {
+            var results = await _context.Actors.ToListAsync();
+            return results;
+        }
+
+        public Actor GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Actor Update(int id, Actor actor)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

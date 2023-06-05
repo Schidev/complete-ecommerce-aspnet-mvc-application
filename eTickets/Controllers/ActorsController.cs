3e2f5ea9558476c9bfd5b97560db22a80d322a9e
Shydeev <hday5297@gmail.com>
2023-06-05 13:27:37 +0300

    Delete actor from db.

--- eTickets/Controllers/ActorsController.cs
+++ eTickets/Controllers/ActorsController.cs
@@ -46,7 +46,7 @@ namespace eTickets.Controllers
             return actorDetails == null ? View("NotFound") : View(actorDetails); 
         }
 
-        // Get: Actors/Edit
+        // Get: Actors/Edit/1
         public async Task<IActionResult> Edit(int id)
         {
             var actorDetails = await _service.GetByIdAsync(id);
@@ -63,6 +63,27 @@ namespace eTickets.Controllers
             }
 
             await _service.UpdateAsync(id, actor);
+            return RedirectToAction(nameof(Index));
+        }
+
+        // Get: Actors/Delete/1
+        public async Task<IActionResult> Delete(int id)
+        {
+            var actorDetails = await _service.GetByIdAsync(id);
+
+            return actorDetails == null ? View("NotFound") : View(actorDetails);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var actorDetails = await _service.GetByIdAsync(id);
+            if (actorDetails == null)
+            {
+                return View("NotFound");
+            }
+
+            await _service.DeleteAsync(id);
             return RedirectToAction(nameof(Index));
         }
     }

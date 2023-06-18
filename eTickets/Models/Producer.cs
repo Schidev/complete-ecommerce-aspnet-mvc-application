66ca82d8a2658d48ab318cf352e28c37dcdb3c61
Shydeev <hday5297@gmail.com>
2023-06-18 10:34:39 +0300

    Add producers.

--- eTickets/Models/Producer.cs
+++ eTickets/Models/Producer.cs
@@ -1,20 +1,26 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using eTickets.Data.Base;
+using System.ComponentModel.DataAnnotations;
 
 namespace eTickets.Models
 {
-    public class Producer
+    public class Producer : IEntityBase
     {
         [Key]
         public int Id { get; set; }
 
         [Display(Name = "Profile picture")]
+        [Required(ErrorMessage = "Profile Picture is required")]
         public string ProfilePictureUrl { get; set; }
+        
         [Display(Name = "Full Name")]
+        [Required(ErrorMessage = "Full Name is required")]
         public string FullName { get; set; }
+        
         [Display(Name = "Biography")]
+        [Required(ErrorMessage = "Biography is required")]
         public string Bio { get; set; }
 
         // Relationships
-        public List<Movie> Movies { get; set; }
+        public List<Movie>? Movies { get; set; }
     }
 }

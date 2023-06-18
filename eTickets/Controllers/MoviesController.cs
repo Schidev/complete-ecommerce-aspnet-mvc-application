210d997a28bc131ac168ba13e71cff0ff50d5e5c
Shydeev <hday5297@gmail.com>
2023-06-18 11:33:56 +0300

    Include method.

--- eTickets/Controllers/MoviesController.cs
+++ eTickets/Controllers/MoviesController.cs
@@ -1,4 +1,5 @@
 ﻿using eTickets.Data;
+using eTickets.Data.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -6,16 +7,16 @@ namespace eTickets.Controllers
 {
     public class MoviesController : Controller
     {
-        private readonly AppDbContext _context;
+        private readonly IMoviesService _service;
 
-        public MoviesController(AppDbContext context)
+        public MoviesController(IMoviesService service)
         {
-            this._context = context;
+            this._service = service;
         }
 
         public async Task<IActionResult> Index()
         {
-            var allMovies = await _context.Movies.Include(c => c.Cinema).OrderBy(m => m.Name).ToListAsync();
+            var allMovies = await _service.GetAllAsync(n => n.Cinema);
 
             return View(allMovies);
         }
